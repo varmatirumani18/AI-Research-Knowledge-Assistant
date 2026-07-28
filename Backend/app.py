4baecf7527bc8df7f2dee876b351ddcb2ecdef9d
varmatirumani18 <varmatirumani18@gmail.com>
2026-07-28 11:16:09 +0530

    Allow all origins in CORS

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,9 @@
-import os
 from flask import Flask
 from flask_cors import CORS
-from routes.api import api_bp
 
 app = Flask(__name__)
-CORS(app)
-
+# Enable CORS for all routes and origins
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
